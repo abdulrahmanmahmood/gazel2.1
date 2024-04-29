ec6fcf5a382c450eb0247208c245f2e0aca83c33
abdulrahmanmahmood <abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2024-04-29 22:34:58 +0300

    edit the header of the news component

--- src/components/News.js
+++ src/components/News.js
@@ -6,7 +6,7 @@ const HomeComp2 = ({ News, Image, title ,inNewspage}) => {
       <div className="flex flex-col lg:flex-row  justify-between px-[3%]  mx-auto ">
         <img src={Image} alt="" className="bg-cover max-w-[50%]  max-h-[50vh]" />
         <div className="flex flex-col w-full lg:w-[45%] ">
-          <h2 className="font-Cairo text-3xl font-bold leading-[67.46px] text-right">
+          <h2 className="font-Cairo text-4xl font-bold leading-[67.46px] text-right mb-3">
             {title}
           </h2>
           <p className="font-Cairo text-2xl font-semibold leading-[44.98px] text-right  line-clamp-[10]">
